583b068b894a38c0adcab6d8e818ef3975387d67
董昊 <donghao@udesk.cn>
2017-11-15 13:52:04 +0800

    add consumers

--- src/nsq_producer/producer.go
+++ src/nsq_producer/producer.go
@@ -4,24 +4,32 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"sync"
 	"time"
 
 	nsq "github.com/nsqio/go-nsq"
 )
 
+var consumers map[string]*nsq.Consumer
+var wg sync.WaitGroup
+
 func StartProduce() {
 	config := nsq.NewConfig()
 	w, _ := nsq.NewProducer("127.0.0.1:4150", config)
+	consumers = make(map[string]*nsq.Consumer)
 
 	count := 1000
+	wg.Add(count)
 
 	for index := 0; index < count; index++ {
 		topic := "nsq_crazy_topic_" + strconv.Itoa(index)
 		fmt.Printf("topic: %s /n", topic)
 
-		NewConsumer(topic)
+		c := NewConsumer(topic)
+		consumers[topic] = c
 
 		go func() {
+			defer wg.Done()
 			for {
 				err := w.Publish(topic, []byte("test"))
 				if err != nil {
@@ -32,5 +40,12 @@ func StartProduce() {
 		}()
 	}
 
+	wg.Wait()
 	defer w.Stop()
+	defer func() {
+		for k, v := range consumers {
+			v.Stop()
+			deleteChannel(k)
+		}
+	}()
 }
