ad3902621406f0665a5c09fc982ccd9704fd6dc8
董昊 <donghao@udesk.cn>
2017-11-17 18:02:37 +0800

    fix

--- src/stress_suit/redis/sub/action/suber.go
+++ src/stress_suit/redis/sub/action/suber.go
@@ -19,6 +19,16 @@ func Stop(res http.ResponseWriter, req *http.Request) {
 	subscribe.Stop()
 }
 
+func Set(res http.ResponseWriter, req *http.Request) {
+	start := queryParam(req, "start")
+	end := queryParam(req, "end")
+	startCount, _ := strconv.Atoi(start)
+	endCount, _ := strconv.Atoi(end)
+	fmt.Printf("Mad producer with count: %d", startCount)
+	fmt.Printf("Mad producer with count: %d", endCount)
+	subscribe.SetCount(startCount, endCount)
+}
+
 func queryParam(req *http.Request, key string) string {
 	return req.URL.Query().Get(key)
 }
